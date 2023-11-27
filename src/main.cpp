56bd5be948dc7eacb93c99a2df49db4215b5d87e
Tahmid A. Khan <tahmidkhan@proton.me>
2023-11-27 06:39:35 +0600

    Implement main compiler executable

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <cstdlib>
+#include <fstream>
+#include <string>
+#include <antlr4-runtime.h>
+#include "ASTBuilderVisitor.h"
+#include "ValgoLexer.h"
+#include "ValgoParser.h"
+
+std::string transpile(antlr4::ANTLRFileStream fileStream)
+{
+    // lexical analysis
+    ValgoLexer lexer{&fileStream};
+    antlr4::CommonTokenStream tokens{&lexer};
+    tokens.fill();
+
+    // syntactic analysis
+    ValgoParser parser{&tokens};
+    ValgoParser::ProgramContext *tree{parser.program()};
+
+    // build AST
+    ASTBuilderVisitor astBuilderVisitor;
+    auto ast = std::any_cast<ProgramAST*>(astBuilderVisitor.visitProgram(tree));
+
+    // generate code
+    SymbolTable symbolTable;
+    return ast->codegen(symbolTable);
+}
+
+
+int main(const int argc, const char* const argv[])
+{
+    if (argc != 3) {
+        std::cerr << "Usage: " << argv[0] << " <input-file.valgo> <output-file>\n";
+        return EXIT_FAILURE;
+    }
+
+    antlr4::ANTLRFileStream fileStream;
+    fileStream.loadFromFile(argv[1]);
+    std::string cxxCode {transpile(fileStream)};
+
+    std::ofstream os {"__tmp.cpp"};
+    std::cerr << cxxCode << std::endl;
+    os << cxxCode;
+
+    std::string command {"g++ -std=c++17 __tmp.cpp -o "};
+    command += argv[2];
+    std::system(command.c_str());
+
+    std::system("rm __tmp.cpp");
+}
